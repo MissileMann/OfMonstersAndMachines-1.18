614f827a85da2cee6f23e0d01e756d2231e7d97e
MissileMann <misael.escoto043@gmail.com>
2022-07-02 18:14:23 -0500

    item registry for new crushed and dust items

--- kubejs/startup_scripts/itemregistry.js
+++ kubejs/startup_scripts/itemregistry.js
@@ -1,3 +1,6 @@
 onEvent('item.registry', event => {
     event.create('rubber_sheet').displayName('Cured Rubber Sheet')
+    event.create('cobalt_dust').displayName('Cobalt Dust')
+    event.create('zinc_dust').displayName('Zinc Dust')
+    event.create('crushed_cobalt_ore').displayName('Crushed Cobalt Ore')
 })
