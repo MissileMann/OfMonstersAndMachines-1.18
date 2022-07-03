7fc1fce157fa2770469965735e8589f1dea3e7af
MissileMann <misael.escoto043@gmail.com>
2022-07-03 01:50:52 -0500

    Added custom collectable cards

--- kubejs/startup_scripts/itemregistry.js
+++ kubejs/startup_scripts/itemregistry.js
@@ -3,4 +3,7 @@ onEvent('item.registry', event => {
     event.create('cobalt_dust').displayName('Cobalt Dust')
     event.create('zinc_dust').displayName('Zinc Dust')
     event.create('crushed_cobalt_ore').displayName('Crushed Cobalt Ore')
+    event.create('collectable_missilecard').displayName('Collectable - Missile`s card').rarity('epic').tooltip('One of the three rare cards you can only find very rarely in End Cities.')
+    event.create('collectable_shinycard').displayName('Collectable - Shiny`s card').rarity('epic').tooltip('One of the three rare cards you can only find very rarely in End Cities.')
+    event.create('collectable_pokelockercard').displayName('Collectable - Pokelocker`s card').rarity('epic').tooltip('One of the three rare cards you can only find very rarely in End Cities.')
 })
