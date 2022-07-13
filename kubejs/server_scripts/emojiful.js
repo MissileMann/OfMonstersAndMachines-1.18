01f50eb1137d0868541297874139856ecf7fab47
MissileMann <misael.escoto043@gmail.com>
2022-07-12 19:22:10 -0500

    emojis

---
+++ kubejs/server_scripts/emojiful.js
@@ -0,0 +1,23 @@
+// priority: 99
+onEvent("recipes", (event) => {
+  event.recipes.emojiful.emoji_recipe({
+    category: "Cool",
+    name: "pack",
+    url: "https://cdn.discordapp.com/emojis/996570878093574194",
+  });
+  event.recipes.emojiful.emoji_recipe({
+    category: "Cool",
+    name: "HUH",
+    url: "https://cdn.discordapp.com/emojis/996569237818724364",
+  });
+  event.recipes.emojiful.emoji_recipe({
+    category: "Cool",
+    name: "OMEGALUL",
+    url: "https://cdn.discordapp.com/emojis/996568934297915514",
+  });
+  event.recipes.emojiful.emoji_recipe({
+    category: "Cool",
+    name: "ratjam",
+    url: "https://tenor.com/view/rat-jam-gif-19408520",
+  });
+});
